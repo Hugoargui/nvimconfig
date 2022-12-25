72965bafdd59f454bc56b603f3734a17e8fcf807
Hugo Arguinariz <harguinariz@7X1XBS2.ra-int.com>
2022-12-25 13:23:34 +0100

    telescope prompt on top was giving warning message

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -1,29 +1,37 @@
 -- import telescope plugin safely
 local telescope_setup, telescope = pcall(require, "telescope")
 if not telescope_setup then
-  return
+	return
 end
 
 -- import telescope actions safely
 local actions_setup, actions = pcall(require, "telescope.actions")
 if not actions_setup then
-  return
+	return
 end
 
 -- configure telescope
 -- require'telescope'.setup{
-telescope.setup{
+telescope.setup({
 	pickers = {
 		colorscheme = {
 			enable_preview = true,
-      file_ignore_patterns = {
-        '%light', '%Light','%latte','dayfox','dawnfox', '%operandi','limestone',
-      'randombones','vimbones'}
-		}
+			file_ignore_patterns = {
+				"%light",
+				"%Light",
+				"%latte",
+				"dayfox",
+				"dawnfox",
+				"%operandi",
+				"limestone",
+				"randombones",
+				"vimbones",
+			},
+		},
 	},
 	defaults = {
-		prompt_prefix = '> ',
-		selection_caret = '> ',
+		prompt_prefix = "> ",
+		selection_caret = "> ",
 		mappings = {
 			i = {
 				["<Esc>"] = actions.close, -- don't go into normal mode, just close
@@ -34,19 +42,17 @@ telescope.setup{
 				["<C-t>"] = actions.select_tab, -- open selection in new tab
 				-- ["<C-y>"] = actions.preview_scrolling_up,
 				-- ["<C-e>"] = actions.preview_scrolling_down
-			}
-		}, -- mappings   
+			},
+		}, -- mappings
 		initial_mode = "insert",
 		selection_strategy = "reset",
 		sorting_strategy = "ascending",
 		layout_strategy = "horizontal",
-		layout_config = {promt_position = "top"}
-
+		-- layout_config = {promt_position = "top"}
 	}, -- defaults
-} --setup
-
+}) --setup
 
 -- telescope.load_extension("fzf")
 --
 --
--- 
+--
