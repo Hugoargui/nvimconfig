4263001d27a55abc3a85d5fdc54bd618fd31de5a
Hugo Arguinariz <you@example.com>
2023-05-22 22:15:37 +0200

    reorder init.lua in more logical order

--- init.lua
+++ init.lua
@@ -5,29 +5,35 @@ require("impatient")
 require("user.core.options")
 require("user.core.keymaps")
 
-require("user.plugins.telescope")
+require("user.plugins.whichkey")
 
-require("user.plugins.comment")
-require("user.plugins.mini-align")
+require("user.plugins.telescope")
 require("user.plugins.nvim-tree")
-require("user.plugins.treesitter-objects")
+
+require("user.plugins.autosession")
+--
+
 require("user.plugins.tint")
 -- require("user.plugins.colorful-winsep")
 -- require("user.plugins.incline")
 require("user.plugins.nvim-cmp")
+require("user.plugins.luasnip")
 
 require("user.plugins.lsp.mason")
 require("user.plugins.lsp.lspsaga")
 require("user.plugins.lsp.lspconfig")
 require("user.plugins.lsp.null-ls")
+require("user.plugins.treesiter")
+
+require("user.plugins.treesitter-objects")
 
 require("user.plugins.autopairs")
+require("user.plugins.comment")
+require("user.plugins.mini-align")
 require("user.plugins.autolist")
-require("user.plugins.treesiter")
-require("user.plugins.indent-blankline")
+
 require("user.plugins.gitsigns")
 
-require("user.plugins.autosession")
 require("user.plugins.lualine")
 -- require("user.plugins.bufferline")
 require("user.plugins.cokeline")
@@ -36,11 +42,9 @@ require("user.plugins.toggleterm")
 
 require("user.plugins.markdown-preview")
 
-require("user.plugins.whichkey")
-
+require("user.plugins.indent-blankline")
 require("user.plugins.eyeliner")
 
 require("user.plugins.colorschemes.catppuccin")
 require("user.core.colorscheme")
-require("user.plugins.luasnip")
 vim.opt.shadafile = ""
