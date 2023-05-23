9b5ed94dafe77c1b531a25599c74158eccc0c958
Hugo Arguinariz <you@example.com>
2023-05-24 00:30:35 +0200

    fix tab key for autocompletion/snippets again, it was interfeering with insert mode tab

--- lua/user/plugins/nvim-cmp.lua
+++ lua/user/plugins/nvim-cmp.lua
@@ -25,21 +25,35 @@ vim.opt.completeopt = "menu,menuone,noselect"
 require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets/vscode" } })
 require("luasnip.loaders.from_lua").lazy_load({ paths = "./snippets/from_lua" })
 
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
-		-- Don't use <CR>, it's annoying that it selects instead of going at a new line when you press enter.
-		-- ["<CR>"] = cmp.mapping.confirm({ select = true }),
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
+cmp.setup({
+	-- snippet = {
+	-- expand = function(args)
+	-- luasnip.lsp_expand(args.body)
+	-- end,
+	-- },
+
+	mapping = cmp.mapping.preset.insert({
+		-- Maybe don't use <CR>, it's annoying that it selects instead of going at a new line when you press enter.
+		-- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<C-Space>"] = cmp.mapping.complete(),
 		["<Tab>"] = cmp.mapping(function(fallback)
+			-- Intellij-like mapping: confirm with tab, and if no entry is selected, confirm first item
 			if cmp.visible() then
+				local entry = cmp.get_selected_entry()
+				if not entry then
+					cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+				else
+					cmp.confirm()
+				end
 				cmp.select_next_item()
-				-- elseif luasnip.expand_or_jumpable() then
-				-- luasnip.expand_or_jump()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
 			elseif has_words_before() then
 				cmp.complete()
 			else
@@ -50,8 +64,8 @@ cmp.setup({
 		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
-				-- elseif luasnip.jumpable(-1) then
-				-- luasnip.jump(-1)
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
 			else
 				fallback()
 			end
@@ -61,14 +75,12 @@ cmp.setup({
 		["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
 		["<C-u>"] = cmp.mapping.scroll_docs(-4),
 		["<C-d>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 		["<C-e>"] = cmp.mapping.abort(), -- close completion window
 	}),
 
 	-- For integrating LuaSnip, from https://sbulav.github.io/vim/neovim-setting-up-luasnip/
 	snippet = {
 		expand = function(args)
-			local luasnip = prequire("luasnip")
 			if not luasnip then
 				return
 			end
