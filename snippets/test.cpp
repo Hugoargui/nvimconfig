751eb942b95d937a28be7147ca806b62d06a993d
Hugo <you@example.com>
2023-06-20 10:30:20 +0200

    Modify test.cpp to play with debugger

--- snippets/test.cpp
+++ snippets/test.cpp
@@ -7,10 +7,10 @@ int foo(void) { return 0; }
 int main() {
   for (int i = 0; i < 5; ++i) {
     if (i != 3) {
-      std::cout << "Hello World " << i << std::endl;
+      int a = foo();
+      std::cout << " Hello World " << i << std::endl;
     }
   }
 
-  int a = foo();
   return 0;
 }
