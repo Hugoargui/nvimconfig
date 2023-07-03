7693114e690f06dbc5b1fabbfb60cd3e4c3cf7eb
Hugo <you@example.com>
2023-07-03 14:02:47 +0200

    Better N works but only after doing / or ?, not on startup. ,; still don't work at all

--- lua/core/plugins/editor/nvim-better-n.lua
+++ lua/core/plugins/editor/nvim-better-n.lua
@@ -1,32 +1,42 @@
 local M = {
     'jonatan-branting/nvim-better-n',
+
     keys = {
-        'n',
-        'N',
-        ';',
-        ',',
+        {
+            'n',
+            function()
+                require('better-n').n()
+            end,
+        },
+        {
+            'N',
+            function()
+                require('better-n').shift_n()
+            end,
+        },
+        { ',' },
+        { ';' },
     },
     config = function()
         require('better-n').setup({
             callbacks = {
-                -- mapping_executed = function()
-                --     -- Clear highlighting, indicating that `n` will not goto the next
-                --     -- highlighted search-term
-                --     vim.cmd([[ nohl ]])
-                -- end,
+                mapping_executed = function()
+                    -- Clear highlighting, indicating that `n` will not goto the next
+                    -- highlighted search-term
+                    vim.cmd([[ nohl ]])
+                end,
             },
             mappings = {
                 -- I want `n` to always go forward, and `<s-n>` to always go backwards
                 ['*'] = { previous = '<s-n>', next = 'n' },
-                ['#'] = { previous = '<s-n>', next = 'n' },
-                ['f'] = { previous = ',', next = ';' },
+                -- TODO: can't get it to work
+                ['#'] = { previous = 'n', next = '<s-n>' },
                 ['F'] = { previous = ',', next = ';' },
-                ['t'] = { previous = ',', next = ';' },
                 ['T'] = { previous = ',', next = ';' },
 
                 -- Setting `cmdline = true` ensures that `n` will only be
                 -- overwritten if the search command is succesfully executed
-                ['/'] = { previous = 'n', next = '<s-n>', cmdline = true },
+                ['/'] = { previous = '<s-n>', next = 'n', cmdline = true },
                 ['?'] = { previous = 'n', next = '<s-n>', cmdline = true },
 
                 -- -- I have <leader>hn/hp bound to git-hunk-next/prev
@@ -38,10 +48,6 @@ local M = {
                 -- ['<leader>bp'] = { previous = '<leader>bp', next = '<leader>bn' },
             },
         })
-        -- You will have to rebind `n` yourself
-        -- vim.keymap.set('n', 'n', require('better-n').n, { nowait = true })
-        vim.keymap.set('n', 'n', require('better-n').n, { nowait = true })
-        vim.keymap.set('n', 'N', require('better-n').shift_n, { nowait = true })
     end,
 }
 
