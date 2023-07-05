6cffdd0fad97965bae85bf9a0c0ece0718e3aa47
Hugo <you@example.com>
2023-07-05 09:33:01 +0200

    Add autocommand to resize splits if window got resized

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -76,6 +76,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     end,
 })
 
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ 'VimResized' }, {
+    callback = function()
+        vim.cmd('tabdo wincmd =')
+    end,
+})
+
 -----------------------------------------------------------
 -- Tabs & Indentation
 -----------------------------------------------------------
