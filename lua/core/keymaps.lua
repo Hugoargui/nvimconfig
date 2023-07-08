6d85eaf28c270be647be22dc09eb81a6ecb8d62d
Hugo <you@example.com>
2023-07-08 18:39:30 +0200

    add L/H mapping for beginning and end of line

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,16 +1,11 @@
 local keymap = vim.keymap --for conciseness
 
--- tab management
--- keymap.set("n", "<leader>to", ":tabnew<CR>", { desc = "Open New Tab" }) -- open new tab
--- keymap.set("n", "<leader>tx", ":tabclose<CR>", { desc = "Close Current Tab" }) -- close current tab
-keymap.set('n', '[t', ':tabn<CR>', { desc = 'Previous Tab' }) --  go to next tab
-keymap.set('n', ']t', ':tabp<CR>', { desc = 'Next Tab' }) --  go to previous tab
-
--- keymap.set('n', 'x', '"_x')
-
 -- Move visual selection up and down
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+vim.keymap.set({ 'n', 'x', 'v', 'o' }, 'L', '$', { desc = 'To end of line' })
+vim.keymap.set({ 'n', 'x', 'v', 'o' }, 'H', '^', { desc = 'To begining of line' })
 
 -- ----------------------------------------------------
 vim.keymap.set('n', '<bs>', 'q', { noremap = true })
