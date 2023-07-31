8e51badf87c5776c4ade57b01256fa7c8213fd82
Hugo <you@example.com>
2023-07-31 18:32:58 +0200

    add markdown snippets

--- snippets/from_lua/markdown.lua
+++ snippets/from_lua/markdown.lua
@@ -4,4 +4,10 @@ local parse = require('luasnip.util.parser').parse_snippet
 return {
     parse('code', '```${1}\n$0\n```'),
     parse('cppcode', '```c++\n$0\n```'),
+    parse('error', '<error>\n$0\n</error>'),
+    parse('warning', '<warning>\n$0\n</warning>'),
+    parse('note', '<note>\n$0\n</note>'),
+    parse('success', '<success>\n$0\n</success>'),
+    parse('info', '<info>\n$0\n</info>'),
+    parse('mark', '<mark>$0</mark>'),
 }
