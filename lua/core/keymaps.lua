cede3edd32cb3e3a6e42b7d25bbf9ddc2ec11729
Hugo <you@example.com>
2023-11-29 20:13:42 +0100

    replace readline plugin by custom mappings

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -23,3 +23,37 @@ vim.keymap.set({ 'n', 'i', 'v' }, '<C-s>', '<Cmd>lua Save_file()<CR>', { noremap
 vim.keymap.set('n', '<leader>z', function()
     require('telescope.builtin').spell_suggest(require('telescope.themes').get_cursor({}))
 end, { desc = '  Spelling Suggestions' })
+
+-- ------------------------------------------------
+-- Insert mode readline mappings
+-- ------------------------------------------------
+
+-- Lines bellow/above
+vim.keymap.set('!', '<M-o>', '<Esc>o') -- LINE BELLOW
+vim.keymap.set('!', '<M-O>', '<Esc>O') -- LINE ABOVE
+
+-- MOVEMENT
+vim.keymap.set('!', '<C-b>', '<Left>') -- ONE CHAR BACK
+vim.keymap.set('!', '<M-b>', '<C-o>b') -- ONE wORD BACK
+
+vim.keymap.set('!', '<C-f>', '<Right>') -- ONE CHAR FRONT
+vim.keymap.set('!', '<M-f>', '<C-o>w') -- ONE WORD FRONT
+
+vim.keymap.set('!', '<C-a>', '<ESC>I') -- GO TO BEGINNING OF LINE
+vim.keymap.set('!', '<C-e>', '<ESC>A') -- GO TO END OF LINE
+
+-- KILL
+vim.keymap.set('!', '<C-h>', '<BS>') -- Delete one character back
+vim.keymap.set('!', '<M-h>', '<C-o>db') -- Delete one word back
+vim.keymap.set('!', '<M-BS>', '<C-o>db') -- Delete one word back
+vim.keymap.set('!', '<C-d>', '<Del>') -- Delete one character front
+vim.keymap.set('!', '<M-d>', '<C-o>dw') -- Delete one character front
+
+vim.keymap.set('!', '<C-k>', '<C-o>d$') -- DELETE TO END OF LINE
+vim.keymap.set('!', '<C-u>', '<C-o>d^') -- DELETE TO BEGINING OF LINE
+vim.keymap.set('!', '<M-l>', '<C-o>dd') -- delete current line
+
+-- TODO: <c-d> removes one level of indentation, mayb emap to <c-<>
+-- TODO: <c-t> adds one level of indentation, mayb emap to <c->>
+
+vim.keymap.set('!', '<M-d>', '<esc>yypgi') -- Duplicate current line bellow
